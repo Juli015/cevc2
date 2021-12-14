da8b2ab1323706c195896488410070f84c0d6ea3
Juli015 <hfjulio8@gmail.com>
2021-12-14 16:43:05 -0500

    button

--- app/src/main/java/com/example/cevc2/MainActivity.java
+++ app/src/main/java/com/example/cevc2/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.cevc2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button next;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.first_fragment);
+        next=(Button)findViewById(R.id.ventana2);
+
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent i = new Intent(MainActivity.this, Next.class);
+                startActivity(i);
+
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/cevc2/Next.java
@@ -0,0 +1,14 @@
+package com.example.cevc2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Next extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.second_fragment);
+    }
+}
