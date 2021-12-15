83907b63269bef24d8f1d4f4885804596a4978c4
Juli015 <hfjulio8@gmail.com>
2021-12-14 19:30:13 -0500

    img

---
+++ app/src/androidTest/java/imgs/ScaleType.java
@@ -0,0 +1,4 @@
+package imgs;
+
+public class ScaleType {
+}
---
+++ app/src/main/java/com/example/cevc2/Img.java
@@ -0,0 +1,53 @@
+package com.example.cevc2;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+public class Img  extends BaseAdapter {
+
+    private Context context;
+    public int[] imgs = {
+            R.drawable.boat1,
+            R.drawable.boat2,
+            R.drawable.boat3,
+
+    };
+
+    public Img(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return imgs.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return imgs[position];
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ImageView imageView = new ImageView(context);
+        imageView.setImageResource(imgs[position]);
+        imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+        imageView.setLayoutParams(
+                new GridView.LayoutParams(
+                        340,
+                        350
+                ));
+
+        return imageView;
+    }
+}
